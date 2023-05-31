53fcc167a3774f505420c2e161725e798a960c15
benduran <stratodyne@gmail.com>
2023-05-30 21:36:18 -0700

    chore: sure hope this doesn't loop

--- src/changeset.js
+++ src/changeset.js
@@ -3,12 +3,15 @@
 import appRootPath from 'app-root-path';
 import fs from 'fs-extra';
 import LZString from 'lz-string';
+import os from 'os';
 import path from 'path';
 
 import { fixCWD } from './cwd.js';
 import { gitAdd, gitAmend, gitCommitsSince, gitRevParse } from './git.js';
 
 export const DEFAULT_CHANGESET_FILE_PATH = path.join(process.cwd(), '.lets-version-changes');
+
+const LOCKFILE_PATH = path.join(os.tmpdir(), '__let-version-post-commit-lock__');
 
 /**
  * Given a git commit that was literally just committed (we're in a post-commit hook execution phase),
@@ -35,6 +38,15 @@ export async function upsertChangeset(filePath = DEFAULT_CHANGESET_FILE_PATH, cw
   /** @type {GitCommit[]} */
   const update = [...(existingChangeset ?? []), commitDetails];
   const duplicatesRemoved = new Map(update.map(u => [u.sha, u]));
+
+  // check to see if we have the lockfile in place to prevent infinite post-commit hook loops
+  try {
+    const stat = await fs.stat(LOCKFILE_PATH);
+    if (stat.isFile()) {
+      await fs.remove(LOCKFILE_PATH);
+      return duplicatesRemoved;
+    }
+  } catch (error) {}
 
   await fs.writeFile(filePath, LZString.compress(JSON.stringify(Array.from(duplicatesRemoved.values()))), 'utf-8');
 
