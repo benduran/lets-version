5d2eb71ed09cc5252aab15ce1d581c7d829c47fe
benduran <stratodyne@gmail.com>
2023-06-07 09:16:31 -0700

    chore: added test coverage reporting

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vitest/config';
 // Just a basic config
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'v8',
+      reporter: ['html', 'json', 'json-summary'],
+      reportsDirectory: './coverage',
+    },
     environment: 'node',
   },
 });
