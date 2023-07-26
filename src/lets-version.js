6b2cb13aefc744e6a9d92b44b8a72f665344dcd4
benduran <stratodyne@gmail.com>
2023-07-26 09:35:40 -0400

    feat(changed-packages): enhanced cmd to print all files changed by package

--- src/lets-version.js
+++ src/lets-version.js
@@ -126,14 +126,22 @@ export async function getChangedPackagesSinceBump(opts) {
   const { names, noFetchTags = false, cwd = appRootPath.toString() } = opts ?? {};
   const fixedCWD = fixCWD(cwd);
 
-  const filteredPackages = await filterPackagesByNames(await getPackages(fixedCWD), names, fixedCWD);
+  const allPackages = await getPackages(fixedCWD);
+  const rootPackage = allPackages.find(p => p.root);
+  const filteredPackages = await filterPackagesByNames(allPackages, names, fixedCWD);
 
   if (!filteredPackages) return [];
 
   const tagInfos = await getLastKnownPublishTagInfoForAllPackages(filteredPackages, noFetchTags, fixedCWD);
   const changedFiles = await getAllFilesChangedSinceTagInfos(tagInfos, fixedCWD);
 
-  return getAllPackagesChangedBasedOnFilesModified(changedFiles, filteredPackages, fixedCWD);
+  /** @type {PackageInfo[]} */
+  const allPackagesFilteredPlusRoot = [...filteredPackages];
+  if (rootPackage) allPackagesFilteredPlusRoot.push(rootPackage);
+
+  const deduped = Array.from(new Map(allPackagesFilteredPlusRoot.map(p => [p.name, p])).values());
+
+  return getAllPackagesChangedBasedOnFilesModified(changedFiles, deduped, fixedCWD);
 }
 
 /**
