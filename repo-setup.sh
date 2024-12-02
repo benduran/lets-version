0800d9557c1119ee8a96f9ade8f409240487fa57
benduran <stratodyne@gmail.com>
2024-12-02 22:03:51 +0000

    chore: fully migrated to bun

--- repo-setup.sh
+++ repo-setup.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-if [ "$CI" != "" ]; then
-  npm ci
-else
-  npm install
+bunExists() {
+  RESULT="$(command -v bun)"
+
+  if [ -z "$RESULT" ]; then
+    echo "not_found"
+  else
+    echo $RESULT
+  fi
+}
+
+BUNPATH="$(bunExists)"
+
+if [ "$BUNPATH" == "not_found" ]; then
+  echo "Bun wasn't found. Downloading now..."
+  # assume a unix-like platform.
+  # sorry Windows folks. catch you next time 👋🏼
+  curl -fsSL https://bun.sh/install | bash
+  exec /usr/bin/zsh
 fi
 
-npx husky
+if [ "$CI" != "" ]; then
+  bun install --frozen-lockfile
+else
+  bun install
+fi
+
+bun husky
